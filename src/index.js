67536e7a3b7c6980d49ab39fe1165981d41aa858
AneteCoding <77099964+AneteCoding@users.noreply.github.com>
2021-01-10 22:46:34 +0100

    cleaning

--- src/index.js
+++ src/index.js
@@ -33,10 +33,6 @@ function currentDate(date) {
 
   return `<strong> ${hrs}:${min} </strong> ${days[date.getDay()]}, ${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
 }
-let currentTime = new Date();
-document.querySelector(".date").innerHTML = currentDate(currentTime);
-
-
 
 function getTimestamp(targetTimestampInSec, targetOffsetInSec) {
   let now = new Date();
@@ -46,33 +42,6 @@ function getTimestamp(targetTimestampInSec, targetOffsetInSec) {
   return now.getTime() + (now.getTimezoneOffset() * 60 * 1000) + (targetOffsetInSec * 1000);
 
 }
-
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday"
-];
-
-
-let months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December"
-];
-
 
 function formatTime(timestamp) {
   let date = new Date(timestamp);
@@ -86,8 +55,6 @@ function formatTime(timestamp) {
   }
   return `${hrs}:${min}`;
 }
-
-
 
 function displayWeather(response) {
 
@@ -110,16 +77,51 @@ function displayWeather(response) {
   document.querySelector(`#humidity`).innerHTML = response.data.main.humidity;
   document.querySelector(`#wind`).innerHTML = Math.round(response.data.wind.speed);
   document.querySelector(`#clouds`).innerHTML = response.data.weather[0].description;
-  document.querySelector(`#sunrise`).innerHTML = formatTime(response.data.sys.sunrise * 1000);
-  document.querySelector(`#sunset`).innerHTML = formatTime(response.data.sys.sunset * 1000);
   document.querySelector(`#icon`).setAttribute(`src`, `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
   document.querySelector(`#icon`).setAttribute(`alt`, response.data.weather[0].description)
 }
 
+function search(city) {
+  let apiKey = `e51c2a6a7756a9cab824e5d6224c7dcc`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?`;
+  let units = `metric`;
+  axios.get(`${apiUrl}q=${city}&units=${units}&appid=${apiKey}`).then(displayWeather);
+  apiUrl = (`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=${units}&appid=${apiKey}`)
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function searchCity(event) {
+  event.preventDefault();
+  let city = document.querySelector(`#search-city`).value;
+  search(city);
+}
+
+function getPosition(event) {
+  event.preventDefault()
+  navigator.geolocation.getCurrentPosition(searchPosition);
+}
+
+function searchPosition(position) {
+  let apiKey = `e51c2a6a7756a9cab824e5d6224c7dcc`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function convertF(event) {
+  event.preventDefault();
+  document.querySelector(`#celsius-unit`).classList.remove(`active`);
+  document.querySelector(`#farenheit-unit`).classList.add(`active`);
+  document.querySelector(`#temperature`).innerHTML = Math.round((celsiusTemp * 9) / 5 + 32);
+}
+
+function convertC(event) {
+  event.preventDefault();
+  document.querySelector(`#celsius-unit`).classList.add(`active`);
+  document.querySelector(`#farenheit-unit`).classList.remove(`active`);
+  document.querySelector(`#temperature`).innerHTML = Math.round(celsiusTemp);
+}
 
 function displayForecast(response) {
-
-  console.log(response.data)
 
   let forecast = null;
   let forecastElement = document.querySelector(`#forecast`);
@@ -142,55 +144,23 @@ function displayForecast(response) {
   }
 }
 
-function search(city) {
-  let apiKey = `e51c2a6a7756a9cab824e5d6224c7dcc`;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?`;
-  let units = `metric`;
-  axios.get(`${apiUrl}q=${city}&units=${units}&appid=${apiKey}`).then(displayWeather);
-
-  apiUrl = (`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=${units}&appid=${apiKey}`)
-  axios.get(apiUrl).then(displayForecast);
-}
-
-function searchCity(event) {
-  event.preventDefault();
-  let city = document.querySelector(`#search-city`).value;
-  search(city);
-}
-document.querySelector(`.search-city`).addEventListener(`submit`, searchCity);
-
-function getPosition(event) {
-  event.preventDefault()
-  navigator.geolocation.getCurrentPosition(searchPosition);
-}
-
-function searchPosition(position) {
-  let apiKey = `e51c2a6a7756a9cab824e5d6224c7dcc`;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&appid=${apiKey}`;
-  axios.get(apiUrl).then(displayWeather);
-}
-
-document.querySelector(`#location`).addEventListener(`click`, getPosition);
-
-function convertF(event) {
-  event.preventDefault();
-  document.querySelector(`#celsius-unit`).classList.remove(`active`);
-  document.querySelector(`#farenheit-unit`).classList.add(`active`);
-  document.querySelector(`#temperature`).innerHTML = Math.round((celsiusTemp * 9) / 5 + 32);
-};
-
-document.querySelector(`#farenheit-unit`).addEventListener(`click`, convertF);
-
-function convertC(event) {
-  event.preventDefault();
-  document.querySelector(`#celsius-unit`).classList.add(`active`);
-  document.querySelector(`#farenheit-unit`).classList.remove(`active`);
-  document.querySelector(`#temperature`).innerHTML = Math.round(celsiusTemp);
-};
-
-document.querySelector(`#celsius-unit`).addEventListener(`click`, convertC);
-
 let celsiusTemp = null;
+let currentTime = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+
+document.querySelector(".date").innerHTML = currentDate(currentTime);
+document.querySelector(`.search-city`).addEventListener(`submit`, searchCity);
+document.querySelector(`#location`).addEventListener(`click`, getPosition);
+document.querySelector(`#farenheit-unit`).addEventListener(`click`, convertF);
+document.querySelector(`#celsius-unit`).addEventListener(`click`, convertC);
 
 search(`Barcelona`);
 
